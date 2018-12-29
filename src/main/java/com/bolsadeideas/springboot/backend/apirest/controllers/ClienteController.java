dec9ec3ca0b82874294c3ea219056c3a57e6e35e
Lizardo <lenz.90@gmail.com>
2018-12-29 18:22:48 -0500

    Se coloca que se ponga como Api no reactiva, porque Angular no soporta, falta investigar.

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteController.java
@@ -5,10 +5,12 @@ import com.bolsadeideas.springboot.backend.apirest.models.service.ClienteService
 import io.reactivex.Flowable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
@@ -16,7 +18,7 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
-    @GetMapping(value = "", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public Flowable<Cliente> findAllCliente() {
 
         return clienteService.findAll();
